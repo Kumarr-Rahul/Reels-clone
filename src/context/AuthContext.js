1abde6c3e92fc32df218845077dd62b7e8e7eb9c
Rahul <Rahulkumar980850@gmail.com>
2022-07-24 03:44:37 +0530

    code updated

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,35 +2,66 @@ import React, { useState, useEffect } from "react";
 import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 export const AuthContext = React.createContext();
-
+// children is an inbuilt prop -> it refers to all the component specified inside 
 export function AuthContextProvider({ children }) {
     let [mainLoader, setMainLoader] = useState(true);
     let [cUser, setUser] = useState(null);
-
     useEffect(() => {
-        
-        onAuthStateChanged(auth, (user) => {
-            //Call whenever Login/logout occur
-            //This help in reload, if user already present -> show user and setUser to user
-            //Without this form will appear evertime while reload
-            //Check user is logged in or not
+        // / /event listener
+        let unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            setUser(user);
-            // ...
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/firebase.User
+                setUser(user);
+                // ...
             } else {
-            // User is signed out
-            setUser(null);
+                // User is signed out
+                // ...
+                setUser(null);
             }
-            setMainLoader(false); //To hide login page while reloading
-        });
-    }, []);
 
-    let value = {cUser};
+            setMainLoader(false);
+
+            return unsubscribe;
+        });
+        return unsubscribe;
+    }, []);
+    let value = cUser
     return (
         <AuthContext.Provider value={value}>
             {mainLoader == false && children}
         </ AuthContext.Provider>
     )
-}   
+}
+
+
+
+// 1. event listener added
+//  2. it will return a function that is used to remove the listener
+// function onAuthStateChanged() {
+//     //
+//     //
+//     //
+//     // dmfb dfnb f  mdnf
+//     return function unsubscribe() {
+//         // call
+//     }
+
+// }
+
+//onAuthStateChanged(auth, (user) => {
+//Call whenever Login/logout occur
+//This help in reload, if user already present -> show user and setUser to user
+//Without this form will appear evertime while reload
+//Check user is logged in or not
+// if (user) {
+//     // User is signed in, see docs for a list of available properties
+//     // https://firebase.google.com/docs/reference/js/firebase.User
+//     setUser(user);
+//     // ...
+//     } else {
+//     // User is signed out
+//     setUser(null);
+//     }
+//     setMainLoader(false); //To hide login page while reloading
+// });
