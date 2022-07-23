86f62c3d87ae6c675bd2fd4b4b231b06791c5a82
Rahul <Rahulkumar980850@gmail.com>
2022-07-24 00:52:15 +0530

    Added firebase db

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,10 @@
 //src -> create firebase.js
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
 import secret from "./secrets";
 
 let app = initializeApp(secret);
 //from above lines we able to include firebase in react app
 export let auth = getAuth(app);
+export const db = getFirestore(app);
