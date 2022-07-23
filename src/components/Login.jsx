0351b8c3f263d28c097cf4a9ba61cde30116b31f
Rahul <Rahulkumar980850@gmail.com>
2022-07-24 03:44:10 +0530

    login page updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,85 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { auth } from "../firebase";
-import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-
+import React, { useState, useEffect } from 'react'
+import { auth, } from "../firebase";
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 function Login() {
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
+
   let [user, setUser] = useState(null);
   let [loader, setLoader] = useState(false);
   let [error, setError] = useState("");
-  let [mainLoader, setMainLoader] = useState(true);
 
-  const trackEmail = (e) => {
+  const trackEmail = function (e) {
     setEmail(e.target.value);
   }
-
-  const trackPassword = (e) => {
+  const trackPassword = function (e) {
     setPassword(e.target.value);
   }
-
   const printDetails = async function () {
     // alert(email + " " + password);
     try {
       setLoader(true);
-      let userCred =  await signInWithEmailAndPassword(auth, email, password);
+      // signin call hoga 
+      let userCred = await
+        signInWithEmailAndPassword(auth, email, password)
+      // console.log(userCred.user);
       setUser(userCred.user);
-    } catch(err) {
+    } catch (err) {
       setError(err.message);
-      //After some time -> remove error message
-      setTimeout( () => {
+      // after some time -> error message remove 
+      setTimeout(() => {
         setError("")
-      },2000)
+      }, 2000)
     }
     setLoader(false);
   }
-
-  const signout = async function() {
+  const signout = async function () {
+    // ye call hoga 
     await signOut(auth);
     setUser(null);
   }
-
   useEffect(() => {
-    
     onAuthStateChanged(auth, (user) => {
-      //Call whenever Login/logout occur
-      //This help in reload, if user already present -> show user and setUser to user
-      //Without this form will appear evertime while reload
-      //Check user is logged in or not
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
-        setUser(user);
+        setUser(user)
         // ...
       } else {
         // User is signed out
+        // ...
         setUser(null);
       }
-      setMainLoader(false); //To hide login page while reloading
+      
     });
-  }, []); //EMPTY ARRAY run once in lifetime
-
+  }, []);
   return (
     <>
-      { 
-        error != "" ? <h1>Error is {error}</h1> : 
-        loader == true ? <h1>Loading...</h1> : 
-        user != null ?
-        <>
-          <button onClick={signout}>Signout</button> 
-          <h1> user is {user.uid} </h1> 
-        </> :  
-        
-        <>
-          <input type="email" onChange = {trackEmail} value={email} placeholder='email'></input>
-          <br></br> {/*use for break*/}
-          <input type="password" onChange = {trackPassword} value={password} placeholder='password'></input>
-          <br></br>
-          <button type="submit" onClick = {printDetails}>Login</button>
-        </> /*if user found then show user details else show form */
-      } 
+      {
+       error != "" ? <h1>Error is {error}</h1> :
+          loader == true ? <h1>...Loading</h1> :
+            user != null ?
+              <>
+                <button
+                  onClick={signout}
+                >Signout</button>
+                <h1>user is {user.uid}</h1>
+              </>
+              :
+              <><input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
+                <br></br>
+                <input type="password" onChange={trackPassword} value={password} placeholder="password"
+                ></input>
+                <br></br>
+                <button type="click" onClick={printDetails}
+                >Login</button></>
+      }
     </>
   )
 }
-
-export default Login  
+export default Login
